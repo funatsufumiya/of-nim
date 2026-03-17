55a168e223f11ed2b4886d564a0e184b40b0ea3b
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2026-03-17 14:55:35 +0900

    feat: add example

---
+++ examples/osc_test.nim
@@ -0,0 +1,23 @@
+import ofApp
+import nimline
+
+{.emit: """
+#include "ofMain.h"
+""" .}
+
+proc setup() {.cdecl.} =
+    discard
+
+proc update() {.cdecl.} =
+    discard
+
+proc draw() {.cdecl.} =
+    discard
+
+proc keyPressed(key: cint) {.cdecl.} =
+    let ckey = cast[char](key)
+    echo "key: ", $ckey
+
+when isMainModule:
+    var app = makeOfApp(setup=setup, update=update, draw=draw, keyPressed=keyPressed)
+    app.run(800, 600)
